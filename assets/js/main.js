a8a7d4a02633c0e709f520e8de70287bd58cc81a
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-21 12:43:49 +0530

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,8 +136,8 @@ const sr = ScrollReveal({
     // reset: true // Animations repeat
  })
 
- sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
+ sr.reveal(`.home__perfil, .about__image, .contact__mail,.qualification,.skills`, {origin: 'right'})
  sr.reveal(`.home__name, .home__info, 
             .about__container .section__title-1, .about__info, 
             .contact__social, .contact__data`, {origin: 'left'})
- sr.reveal(`.services__card, .projects__card`, {interval: 100})
+ sr.reveal(`.services__card, .projects__card,.skills__blob,.qualification__name`, {interval: 100})
